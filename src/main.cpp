e30969b9d355da9ad7e0a8a6d5f70f796830b2d7
Ephraim Siegfried <ephraim.siegfried@hotmail.com>
2024-07-09 15:50:12 +0200

    implement basic lora sender

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,39 @@
 #include <Arduino.h>
+#include <LoRa.h>
+#include <Wire.h>
+
+#define SCK  5   // GPIO5  -- SX1278's SCK
+#define MISO 19  // GPIO19 -- SX1278's MISnO
+#define MOSI 27  // GPIO27 -- SX1278's MOSI
+#define SS   18  // GPIO18 -- SX1278's CS
+#define RST  14  // GPIO14 -- SX1278's RESET
+#define DI0  26  // GPIO26 -- SX1278's IRQ(Interrupt Request)
+#define BAND 868E6
+
+unsigned int counter = 0;
 
 void setup() {
-  Serial.begin(9600);
-  while (!Serial)
-    ;
-  Serial.println();
-  Serial.println("LoRa Sender Test");
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(14, OUTPUT);
+    Serial.begin(9600);
+    while (!Serial)
+        ;
+
+    SPI.begin(SCK, MISO, MOSI, SS);
+    LoRa.setPins(SS, RST, DI0);
+    if (!LoRa.begin(BAND)) {
+        Serial.println("Starting LoRa failed!");
+        while (1)
+            ;
+    }
+    Serial.println("Succesfully Initialized");
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(14, HIGH); // turn the LED on (HIGH is the voltage level)
-  delay(5000);            // wait for a second
-  Serial.println("yooo");
-  digitalWrite(14, LOW); // turn the LED off by making the voltage LOW
-  delay(5000);           // wait for a second
+    LoRa.beginPacket();
+    String packet = "hello " + String(counter);
+    Serial.println("Sending packet: " + packet);
+    LoRa.print(packet);
+    LoRa.endPacket();
+    counter++;
+    delay(3000);
 }
