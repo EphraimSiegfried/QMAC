1d484b203db762046e82dae1b262abd52eb84924
Ephraim Siegfried <e.siegfried@stud.unibas.ch>
2024-07-14 10:33:00 +0200

    fix listen-only env

--- playground/LoRaReceiver.cpp
+++ playground/LoRaReceiver.cpp
@@ -1,12 +1,9 @@
 #include <LoRa.h>
-#include <SPI.h>
 
 #define SS   18  // GPIO18 -- SX1278's CS
 #define RST  14  // GPIO14 -- SX1278's RESET
 #define DI0  26  // GPIO26 -- SX1278's IRQ(Interrupt Request)
 #define BAND 868E6
-
-void onReceive(int packetSize);
 
 void setup() {
     Serial.begin(9600);
